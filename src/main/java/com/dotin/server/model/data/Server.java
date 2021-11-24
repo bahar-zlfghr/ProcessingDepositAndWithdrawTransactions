908ac17d5dfcd5b12659be81a6f58bc74184e695
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-11-24 21:14:31 +0330

    deposits list removed

--- src/main/java/com/dotin/server/model/data/Server.java
+++ src/main/java/com/dotin/server/model/data/Server.java
@@ -3,9 +3,6 @@ package com.dotin.server.model.data;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author : Bahar Zolfaghari
  **/
@@ -13,9 +10,6 @@ public class Server {
 
     @Getter @Setter
     private Integer port;
-
-    @Getter @Setter
-    private List<Deposit> deposits = new ArrayList<>();
 
     @Getter @Setter
     private String outLogPath;
