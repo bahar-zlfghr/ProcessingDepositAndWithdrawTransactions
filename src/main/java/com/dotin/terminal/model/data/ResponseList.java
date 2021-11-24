8d7b37016f0ca3fee93907baf9d78372cd161d23
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-11-24 21:18:28 +0330

    response list class created for save responses in the .xml file

---
+++ src/main/java/com/dotin/terminal/model/data/ResponseList.java
@@ -0,0 +1,15 @@
+package com.dotin.terminal.model.data;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author : Bahar Zolfaghari
+ **/
+public class ResponseList {
+
+    @Getter
+    private static final List<Response> responses = new ArrayList<>();
+}
