b2e2792ba7b9dbebb3334985ac731c30bdd36298
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-11-24 21:07:41 +0330

    implementation changed to get a document

--- src/main/java/com/dotin/terminal/util/DocumentUtil.java
+++ src/main/java/com/dotin/terminal/util/DocumentUtil.java
@@ -8,44 +8,15 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * @author : Bahar Zolfaghari
  **/
 public abstract class DocumentUtil {
-    private final static String ROOT = "src/main/resources";
+    private final static String ROOT = "src/main/resources/terminal/";
 
-    public static Set<Document> getDocuments() throws IOException {
-        Set<String> fileNames = getFileNames();
-        return fileNames.stream().map(fileName -> {
-            Document document = null;
-            try {
-                document = createDocument(fileName);
-            } catch (ParserConfigurationException | IOException | SAXException e) {
-                e.printStackTrace();
-            }
-            return document;
-        }).collect(Collectors.toSet());
-    }
-
-    private static Set<String> getFileNames() throws IOException {
-        try (Stream<Path> pathStream = Files.walk(Paths.get(ROOT + "/terminal"), 1)) {
-            return pathStream
-                    .filter(file -> !Files.isDirectory(file))
-                    .map(Path::getFileName)
-                    .map(Path::toString)
-                    .collect(Collectors.toSet());
-        }
-    }
-
-    private static Document createDocument(String fileName) throws ParserConfigurationException, IOException, SAXException {
-        File file = new File(ROOT + "/terminal", fileName);
+    public static Document createDocument(String fileName) throws ParserConfigurationException, IOException, SAXException {
+        File file = new File(ROOT +  fileName);
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         return builder.parse(file);
