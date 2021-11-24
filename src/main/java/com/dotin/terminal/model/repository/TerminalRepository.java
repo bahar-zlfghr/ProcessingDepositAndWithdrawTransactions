c72ebddbe4eea762de613d7bf5d00cfbbf55623f
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-11-24 21:09:25 +0330

    terminal list removed and fetch just one terminal info

--- src/main/java/com/dotin/terminal/model/repository/TerminalRepository.java
+++ src/main/java/com/dotin/terminal/model/repository/TerminalRepository.java
@@ -6,98 +6,65 @@ import lombok.Getter;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 
 /**
  * @author : Bahar Zolfaghari
  **/
-public abstract class TerminalRepository {
+public class TerminalRepository {
 
     @Getter
-    private static final List<Terminal> terminals = new ArrayList<>();
+    private static final Terminal terminal = new Terminal();
 
-    static {
-        fetchAllTerminals();
-    }
-
-    public static void fetchAllTerminals() {
+    public static void fetchTerminal(String terminalFileName) {
         try {
-            Set<Document> documents = DocumentUtil.getDocuments();
-            documents.forEach(document -> {
-                Node terminalNode = document.getElementsByTagName("terminal").item(0);
-                Terminal terminal = fetchTerminal(terminalNode);
-
-                Node serverNode = document.getElementsByTagName("server").item(0);
-                Server server = fetchServer(serverNode);
-                terminal.setServer(server);
-
-                Node outLogNode = document.getElementsByTagName("outLog").item(0);
-                String outLogPath = fetchLogFileName(outLogNode);
-                terminal.setOutLogPath(outLogPath);
-
-                NodeList transactionNodes = document.getElementsByTagName("transaction");
-                for (int i = 0; i < transactionNodes.getLength(); i++) {
-                    Node transactionNode = transactionNodes.item(i);
-                    Transaction transaction = fetchTransaction(transactionNode);
-                    terminal.getTransactions().add(transaction);
-                }
-                terminals.add(terminal);
-            });
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
+            Document document = DocumentUtil.createDocument(terminalFileName);
+            Node terminalNode = document.getElementsByTagName("terminal").item(0);
+            setTerminalInfo(terminalNode);
+            Node serverNode = document.getElementsByTagName("server").item(0);
+            setServerInfo(serverNode);
+            Node outLogNode = document.getElementsByTagName("outLog").item(0);
+            setLogFileNameInfo(outLogNode);
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 
-    private static Terminal fetchTerminal(Node terminalNode) {
-        Terminal terminal = null;
+    private static void setTerminalInfo(Node terminalNode) throws Exception {
         if (terminalNode.getNodeType() == Node.ELEMENT_NODE) {
             Element transactionElement = (Element) terminalNode;
             String id = transactionElement.getAttribute("id");
-            //TODO: terminal type error
             TerminalType type = TerminalType.getTerminalType(transactionElement.getAttribute("type"));
-            terminal = new Terminal(id, type);
+            if (type == null) {
+                throw new Exception("TerminalTypeMismatchException");
+            }
+            else {
+                terminal.setId(id);
+                terminal.setType(type);
+            }
         }
-        return terminal;
     }
 
-    private static Server fetchServer(Node serverNode) {
-        Server server = null;
+    private static void setServerInfo(Node serverNode) {
         if (serverNode.getNodeType() == Node.ELEMENT_NODE) {
             Element transactionElement = (Element) serverNode;
-            String ip = transactionElement.getAttribute("ip");
-            String port = transactionElement.getAttribute("port");
-            server = new Server(ip, port);
+            String serverIP = transactionElement.getAttribute("ip");
+            Integer serverPort = Integer.valueOf(transactionElement.getAttribute("port"));
+            terminal.setServerIP(serverIP);
+            terminal.setServerPort(serverPort);
         }
-        return server;
     }
 
-    private static String fetchLogFileName(Node outLogNode) {
-        String outLog = "";
+    private static void setLogFileNameInfo(Node outLogNode) {
         if (outLogNode.getNodeType() == Node.ELEMENT_NODE) {
             Element transactionElement = (Element) outLogNode;
-            outLog = transactionElement.getAttribute("path");
+            String outLogPath = transactionElement.getAttribute("path");
+            terminal.setOutLogPath(outLogPath);
         }
-        return outLog;
-    }
-
-    private static Transaction fetchTransaction(Node transactionNode) {
-        Transaction transaction = null;
-        if (transactionNode.getNodeType() == Node.ELEMENT_NODE) {
-            Element transactionElement = (Element) transactionNode;
-            String id = transactionElement.getAttribute("id");
-            //TODO: transaction type error
-            TransactionType type = TransactionType.getTransactionType(transactionElement.getAttribute("type"));
-            //TODO: transaction amount error
-            BigDecimal amount = new BigDecimal(transactionElement.getAttribute("amount"));
-            String depositID = transactionElement.getAttribute("deposit");
-            transaction = new Transaction(id, type, amount, depositID);
-        }
-        return transaction;
     }
 }
