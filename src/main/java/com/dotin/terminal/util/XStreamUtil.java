c525c59f9a0def2ce7600962c6a6d6c814976aea
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-11-24 21:20:34 +0330

    xml util class created for initialize XStream

---
+++ src/main/java/com/dotin/terminal/util/XStreamUtil.java
@@ -0,0 +1,18 @@
+package com.dotin.terminal.util;
+
+import com.dotin.terminal.model.data.Response;
+import com.dotin.terminal.model.data.ResponseList;
+import com.thoughtworks.xstream.XStream;
+
+/**
+ * @author : Bahar Zolfaghari
+ **/
+public class XStreamUtil {
+
+    public static XStream createXStream() {
+        XStream xStream = new XStream();
+        xStream.alias("responses", ResponseList.class);
+        xStream.alias("response", Response.class);
+        return xStream;
+    }
+}
