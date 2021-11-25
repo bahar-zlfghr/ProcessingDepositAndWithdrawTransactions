56348eacd6789a97e124183bc155eee18efdc891
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-11-25 13:13:01 +0330

    terminal log file class created for save terminal log file path and provide global access to it

---
+++ src/main/java/com/dotin/terminal/model/data/TerminalLogFile.java
@@ -0,0 +1,13 @@
+package com.dotin.terminal.model.data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author : Bahar Zolfaghari
+ **/
+public class TerminalLogFile {
+
+    @Getter @Setter
+    private static String logFilePath;
+}
