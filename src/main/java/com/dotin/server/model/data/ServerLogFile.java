903018ec838308d0bc573d5687f00a5840b02120
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-11-25 13:13:21 +0330

    server log file class created for save server log file path and provide global access to it

---
+++ src/main/java/com/dotin/server/model/data/ServerLogFile.java
@@ -0,0 +1,13 @@
+package com.dotin.server.model.data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author : Bahar Zolfaghari
+ **/
+public class ServerLogFile {
+
+    @Getter @Setter
+    private static String logFilePath;
+}
