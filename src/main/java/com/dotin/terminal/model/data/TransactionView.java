8a1765439949e08b9c5df4b9d133e903b107e724
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-11-30 12:18:30 +0330

    create transaction view from transaction element function added

--- src/main/java/com/dotin/terminal/model/data/TransactionView.java
+++ src/main/java/com/dotin/terminal/model/data/TransactionView.java
@@ -1,7 +1,9 @@
 package com.dotin.terminal.model.data;
 
+import com.dotin.terminal.validation.TransactionValidation;
 import lombok.Getter;
 import lombok.Setter;
+import org.w3c.dom.Element;
 
 import java.math.BigDecimal;
 
@@ -28,5 +30,16 @@ public class TransactionView {
         this.type = type;
         this.amount = amount;
         this.depositID = depositID;
+    }
+
+    public static TransactionView createTransactionViewFromTransactionElement(Element transactionElement) {
+        String id = transactionElement.getAttribute("id");
+        TransactionValidation.transactionUniqueIDValidation(id);
+        TransactionType type = TransactionType.getTransactionType(transactionElement.getAttribute("type"));
+        TransactionValidation.transactionTypeValidation(type);
+        BigDecimal amount = new BigDecimal(transactionElement.getAttribute("amount"));
+        TransactionValidation.transactionAmountValidation(amount);
+        String depositID = transactionElement.getAttribute("deposit");
+        return new TransactionView(id, type, amount, depositID);
     }
 }
