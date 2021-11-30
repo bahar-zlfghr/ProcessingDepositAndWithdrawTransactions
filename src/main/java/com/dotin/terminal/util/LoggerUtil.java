ddb478a946a2c8dad1c7ade2b87df37855ac2019
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-11-30 12:19:24 +0330

    exceptions log function added

--- src/main/java/com/dotin/terminal/util/LoggerUtil.java
+++ src/main/java/com/dotin/terminal/util/LoggerUtil.java
@@ -1,6 +1,10 @@
 package com.dotin.terminal.util;
 
+import com.dotin.terminal.model.data.TerminalLogFile;
+import com.dotin.terminal.model.repository.TransactionRepository;
 import org.apache.log4j.Logger;
+
+import java.util.List;
 
 /**
  * @author : Bahar Zolfaghari
@@ -10,5 +14,10 @@ public interface LoggerUtil {
     static Logger getLogger(Class<?> clazz, String path) {
         System.setProperty("LogFilePath", path);
         return Logger.getLogger(clazz);
+    }
+
+    static void exceptionsLog(List<Exception> exceptions) {
+        Logger logger = LoggerUtil.getLogger(TransactionRepository.class, TerminalLogFile.getLogFilePath());
+        exceptions.forEach(e -> logger.error(e.getMessage(), e));
     }
 }
