da21b1f4678b60ecfd1e2cbf479febfab435b3b6
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-11-30 09:13:47 +0330

    minor changes

--- src/main/java/com/dotin/server/service/TransactionViewService.java
+++ src/main/java/com/dotin/server/service/TransactionViewService.java
@@ -1,5 +1,6 @@
 package com.dotin.server.service;
 
+import com.dotin.server.model.data.TransactionType;
 import com.dotin.server.model.data.TransactionView;
 
 import java.math.BigDecimal;
@@ -11,7 +12,7 @@ public interface TransactionViewService {
 
     static TransactionView createTransactionView(String data) {
         String[] tokens = data.split(", ");
-        String type = tokens[0];
+        TransactionType type = TransactionType.valueOf(tokens[0]);
         BigDecimal amount = new BigDecimal(tokens[1]);
         String depositID = tokens[2];
         return new TransactionView(type, amount, depositID);
