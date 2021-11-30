8707db708bae1116f6c92d4e5d1552b0bf6497d9
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-11-30 12:21:09 +0330

    list of exceptions added for log all exceptions of the transactions

--- src/main/java/com/dotin/terminal/model/repository/TransactionRepository.java
+++ src/main/java/com/dotin/terminal/model/repository/TransactionRepository.java
@@ -1,10 +1,7 @@
 package com.dotin.terminal.model.repository;
 
-import com.dotin.terminal.exception.TransactionAmountInvalidException;
-import com.dotin.terminal.exception.TransactionTypeMismatchException;
 import com.dotin.terminal.model.data.TerminalLogFile;
 import com.dotin.terminal.model.data.Transaction;
-import com.dotin.terminal.model.data.TransactionType;
 import com.dotin.terminal.model.data.TransactionView;
 import com.dotin.terminal.util.DocumentUtil;
 import com.dotin.terminal.util.LoggerUtil;
@@ -18,7 +15,6 @@ import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,6 +26,9 @@ public abstract class TransactionRepository {
     @Getter
     private static final List<Transaction> transactions = new ArrayList<>();
 
+    @Getter
+    private static final List<Exception> exceptions = new ArrayList<>();
+
     public static void fetchTransactions(String terminalFileName) {
         Logger logger = LoggerUtil.getLogger(TransactionRepository.class, TerminalLogFile.getLogFilePath());
         try {
@@ -39,36 +38,20 @@ public abstract class TransactionRepository {
                 Node transactionNode = transactionNodes.item(i);
                 if (transactionNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element transactionElement = (Element) transactionNode;
-                    TransactionView transactionView = createTransactionViewFromTransactionElement(transactionElement);
+                    TransactionView transactionView = TransactionView.createTransactionViewFromTransactionElement(transactionElement);
                     Transaction transaction = new Transaction(transactionView);
-                    transactions.add(transaction);
+                    if (exceptions.isEmpty()) {
+                        transactions.add(transaction);
+                    }
+                    else {
+                        LoggerUtil.exceptionsLog(exceptions);
+                    }
+                    exceptions.clear();
                 }
             }
-        } catch (ParserConfigurationException | IOException | SAXException | TransactionTypeMismatchException | TransactionAmountInvalidException e) {
+        } catch (ParserConfigurationException | IOException | SAXException e) {
             logger.error(e.getMessage(), e);
         }
         logger.info("Transactions info fetched successfully");
-    }
-
-    private static TransactionView createTransactionViewFromTransactionElement(Element transactionElement) throws TransactionTypeMismatchException, TransactionAmountInvalidException {
-        String id = transactionElement.getAttribute("id");
-        TransactionType type = TransactionType.getTransactionType(transactionElement.getAttribute("type"));
-        transactionTypeValidation(type);
-        BigDecimal amount = new BigDecimal(transactionElement.getAttribute("amount"));
-        transactionAmountValidation(amount);
-        String depositID = transactionElement.getAttribute("deposit");
-        return new TransactionView(id, type, amount, depositID);
-    }
-
-    private static void transactionTypeValidation(TransactionType type) throws TransactionTypeMismatchException {
-        if (type == null) {
-            throw new TransactionTypeMismatchException("TransactionTypeMismatchException");
-        }
-    }
-
-    private static void transactionAmountValidation(BigDecimal amount) throws TransactionAmountInvalidException {
-        if (amount.compareTo(BigDecimal.ZERO) < 0) {
-            throw new TransactionAmountInvalidException("The transaction amount less than zero!");
-        }
     }
 }
